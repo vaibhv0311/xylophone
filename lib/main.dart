04fdbd57950a3484fc89186dd9dfe1a833b39d73
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-11 22:05:08 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(onPressed: () {}, child: Text('Press me')),
+          ),
+        ),
+      ),
+    );
+  }
+}
