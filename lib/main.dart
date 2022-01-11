35e5f64e4e2ca007af9f4601c8a1ee3d1bb01df4
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-11 23:18:40 +0530

    Base layout added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
-import 'package:audioplayer/audioplayer.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +9,89 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp() : super();
 
+  void playSound(num) {
+    final audioPlayer = AudioCache();
+    audioPlayer.play('note$num.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-                onPressed: () {
-                  AudioPlayer audioPlayer = AudioPlayer();
-                  audioPlayer.play('/assets/note2.wav');
-                },
-                child: Text('Press me')),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue[900],
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.deepPurpleAccent,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
